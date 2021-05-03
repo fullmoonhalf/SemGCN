b755de059acabc51078ac471195a195d0c5a1f49
fullmoonhalf <fullmoonhalf@users.noreply.github.com>
2021-05-03 20:27:47 +0900

    device and SEM controller

--- mmpose_integration.py
+++ mmpose_integration.py
@@ -1,6 +1,9 @@
 import argparse
 import cv2
 import time
+import torch
+import os.path as path
+
 
 from mmdet.apis import inference_detector, init_detector
 from mmpose.apis import inference_top_down_pose_model, init_pose_model, vis_pose_result
@@ -21,6 +24,14 @@ def parse_args():
     parser.add_argument('--mmp-bbox-thr', type=float, default=0.3, help='Bounding box score threshold')
     parser.add_argument('--mmp-kpt-thr', type=float, default=0.3, help='Keypoint score threshold')
 
+    parser.add_argument('--sem-dropout', default=0.0, type=float, help='dropout rate')
+    parser.add_argument('--sem-num_layers', default=4, type=int, metavar='N', help='num of residual layers')
+    parser.add_argument('--sem-hid_dim', default=128, type=int, metavar='N', help='num of hidden dimensions')
+    parser.add_argument('--sem-evaluate', default='', type=str, metavar='FILENAME', required=True, help='checkpoint to evaluate (file name)')
+
+    parser.add_argument('--device', default='cuda:0', help='Device used for inference')
+
+
     args = parser.parse_args()
     return args
 
@@ -36,8 +47,9 @@ def main(args):
     else:
         cap = cv2.VideoCapture(args.cv_video_path)
 
-    mmpose_driver = MMPoseDriver(args)
-    semgcm_driver = SemGCMDriver()
+    device_driver = DeviceDriver(args)
+    mmpose_driver = MMPoseDriver(args, device_driver)
+    semgcm_driver = SemGCMDriver(args, device_driver)
 
     # main loop.
     print("Main Loop")
@@ -67,12 +79,20 @@ def main(args):
     cv2.destroyAllWindows()
 
 
+class DeviceDriver:
+    def __init__(self, args):
+        print("Initialize DeviceDriver - begin.")
+        self.name = args.device if torch.cuda.is_available() else "cpu"
+        print("Initialize DeviceDriver - device {}".format(self.name))
+        self.device = torch.device(self.name)
+        print("Initialize DeviceDriver - end.")
+
 
 class MMPoseDriver:
-    def __init__(self, args):
+    def __init__(self, args, device):
         print("Initialize MMPoseDriver - begin.")
-        self.det_model = init_detector(args.mmp_det_config, args.mmp_det_checkpoint, device=args.mmp_device)
-        self.pose_model = init_pose_model(args.mmp_pose_config, args.mmp_pose_checkpoint, device=args.mmp_device)
+        self.det_model = init_detector(args.mmp_det_config, args.mmp_det_checkpoint, device=device.name)
+        self.pose_model = init_pose_model(args.mmp_pose_config, args.mmp_pose_checkpoint, device=device.name)
         self.dataset = self.pose_model.cfg.data['test']['type']
         self.bbox_thr = args.mmp_bbox_thr
         self.kpt_thr = args.mmp_kpt_thr
@@ -122,11 +142,60 @@ class MMPoseDriver:
 
 
 class SemGCMDriver:
-    def __init__(self):
-        pass
+    def __init__(self, args, device):
+        print("Initialize SemGCMDriver - begin.")
+        from models.sem_gcn import SemGCN
+        from common.graph_utils import adj_mx_from_skeleton
+        from common.skeleton import Skeleton
+        self.hid_dim = args.sem_hid_dim
+        self.num_layers = args.sem_num_layers
+        self.p_dropout = (None if args.sem_dropout == 0.0 else args.sem_dropout)
+        self.skeleton = Skeleton(
+            parents=[-1, 0, 1, 2, 0, 4, 5, 0, 7, 8, 8, 10, 11, 8, 13, 14 ],
+            joints_left=[4, 5, 6, 10, 11, 12],
+            joints_right=[1, 2, 3, 13, 14, 15]
+        )
+        self.skeleton._joints_group = [[2, 3], [5, 6], [1, 4], [0, 7], [8, 9], [14, 15], [11, 12], [10, 13]]
+        adj = adj_mx_from_skeleton(self.skeleton)
+        self.device = device
+        self.model_pos = SemGCN(
+            adj, 
+            self.hid_dim, 
+            num_layers=self.num_layers, 
+            p_dropout=self.p_dropout,
+            nodes_group=self.skeleton.joints_group()
+        ).to(self.device.device)
+
+        # Resume from a checkpoint
+        ckpt_path = args.sem_evaluate
+        if path.isfile(ckpt_path):
+            print("==> Loading checkpoint '{}'".format(ckpt_path))
+            self.ckpt = torch.load(ckpt_path)
+            self.rename_nonlocal_node(self.ckpt)
+            start_epoch = self.ckpt['epoch']
+            error_best = self.ckpt['error']
+            self.model_pos.load_state_dict(self.ckpt['state_dict'])
+            print("==> Loaded checkpoint (Epoch: {} | Error: {})".format(start_epoch, error_best))
+        else:
+            raise RuntimeError("==> No checkpoint found at '{}'".format(ckpt_path))
+        print("Initialize SemGCMDriver - end.")
+
     def update(self):
         pass
 
+    def rename_nonlocal_node(self, dic):
+        if 'items' not in dir(dic):
+            return
+        targets = []
+        for key, value in dic.items():
+            if type(key) == str:
+                if '.nonlocal.' in key:
+                    targets.append(key)
+            self.rename_nonlocal_node(value)
+        for key in targets:
+            value = dic.pop(key)
+            key = key.replace('.nonlocal.','._nonlocal.')
+            dic[key] = value
 
 
 if __name__ == '__main__':
